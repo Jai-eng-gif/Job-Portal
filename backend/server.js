3080ef4319a8995f7e8f6f3fd73984222b2e91c9
Jai Gawande <gawande20jai@gmail.com>
2025-04-20 22:31:02 +0530

    chng

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express()
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(bodyParser.json()) 
 app.use(cors());
 
