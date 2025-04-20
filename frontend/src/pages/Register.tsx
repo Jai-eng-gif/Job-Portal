6bd41ef076e1e795be7e16cac42166a767d0153f
Jai Gawande <gawande20jai@gmail.com>
2025-04-20 20:36:24 +0530

    chngs

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -18,7 +18,7 @@ function Register() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
+      const response = await fetch("https://job-portal-backend-two-ashen.vercel.app/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
